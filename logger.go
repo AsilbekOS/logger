01585ef00e9ba0ce18d80a82211c180898d3b329
Asilbek Xolmatov <asilbekxolmatov2002@gmail.com@gmail.com>
2025-08-20 20:07:38 +0500

    first release

--- logger.go
+++ logger.go
@@ -1,6 +1,3 @@
-// Shu taritib yozishni boshlang.
-// logger.UseLogger()
-// defer logger.Close()
 package logger
 
 import (
@@ -10,12 +7,14 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// log - global o'zgaruvchi (umumiy logger instance)
+// instance - struct (umumiy logger instance)
 // Bitta marta yaratiladi va butun loyihada foydalaniladi
-var log *zap.Logger
+type Logger struct {
+	instance *zap.Logger
+}
 
 // UseLogger - logger konfiguratsiyasini sozlaydi va ishga tushiradi
-func UseLogger() {
+func UseLogger() *Logger {
 	// zap konfiguratsiyasi
 	cfg := zap.Config{
 		Encoding:         "json",                                  // log format: json yoki console
@@ -35,7 +34,7 @@ func UseLogger() {
 			EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 				enc.AppendString(t.Format("2006-01-02 15:04:05"))
 			},
-			EncodeCaller: zapcore.FullCallerEncoder, // fayl nomi + qator raqamini qisqa formatda chiqarish
+			EncodeCaller: zapcore.ShortCallerEncoder, // fayl nomi + qator raqamini qisqa formatda chiqarish
 		},
 	}
 
@@ -46,43 +45,47 @@ func UseLogger() {
 	}
 
 	// global log o'zgaruvchisini ishga tushirish
-	log = logger
+	return &Logger{instance: logger}
 }
 
-// Close - log faylni yopadi va xotirani tozalaydi
+// Close - log faylni yopadi va xotirani tozalaydi.
 // Asosan main() ichida defer bilan ishlatiladi
-func Close() {
-	_ = log.Sync()
+func (l *Logger) Close() {
+	_ = l.instance.Sync()
 }
 
 // Wrapper funksiyalar - log.Info(), log.Error() kabi qisqa qilib ishlatish uchun
 
 // Info darajadagi log yozish
-func Info(msg string, fields ...zap.Field) {
-	log.Info(msg, fields...)
-	//  Qo'shimcha maydonlar bilan ma'lumot xabarini kiriting
-	// 	logger.Info("User logged in",
-	// 		zap.String("username", "johndoe"),
-	// 		zap.Int("user_id", 42),
-	// 		zap.String("role", "admin"),
-	// )
+//
+//	 Qo'shimcha maydonlar bilan ma'lumot xabarini kiriting
+//		logger.Info("User logged in",
+//			zap.String("username", "johndoe"),
+//			zap.Int("user_id", 42),
+//			zap.String("role", "admin"),
+//
+// )
+func (l *Logger) Info(msg string, fields ...zap.Field) {
+	l.instance.Info(msg, fields...)
 }
 
 // Error darajadagi log yozish
-func Error(msg string, fields ...zap.Field) {
-	log.Error(msg, fields...)
-	// Qo'shimcha maydonlar bilan xatolik xabarini kiriting
-	// logger.Error("Failed to process request",
-	// 		zap.String("request_id", "12345"),
-	// )
+//
+//	 Qo'shimcha maydonlar bilan xatolik xabarini kiriting
+//		logger.Error("Failed to process request",
+//			zap.String("request_id", "12345"),
+//
+// )
+func (l *Logger) Error(msg string, fields ...zap.Field) {
+	l.instance.Error(msg, fields...)
 }
 
 // Warn darajadagi log yozish
-func Warn(msg string, fields ...zap.Field) {
-	log.Warn(msg, fields...)
+func (l *Logger) Warn(msg string, fields ...zap.Field) {
+	l.instance.Warn(msg, fields...)
 }
 
 // Debug darajadagi log yozish (faqat Debug mode uchun)
-func Debug(msg string, fields ...zap.Field) {
-	log.Debug(msg, fields...)
+func (l *Logger) Debug(msg string, fields ...zap.Field) {
+	l.instance.Debug(msg, fields...)
 }
